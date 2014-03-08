71af6184faaf89e94e7d46a92fef1638dd0f73fc
Michele Facchetti <michele.facchetti@gmail.com>
2014-03-08 15:11:10 +0100

    first commit

---
+++ Empty Window/MyRedButton.h
@@ -0,0 +1,13 @@
+//
+//  MyRedButton.h
+//  Empty Window
+//
+//  Created by Michele Facchetti on 08/03/14.
+//  Copyright (c) 2014 Michele Facchetti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyRedButton : UIButton
+
+@end
